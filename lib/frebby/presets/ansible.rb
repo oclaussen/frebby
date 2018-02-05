922842ea311d1f2efb6f4196c11b752425debaad
Ole Claussen <claussen.ole@gmail.com>
2018-02-05 18:56:16 +0100

    terraform preset and ansible pre/post tasks

--- lib/frebby/presets/ansible.rb
+++ lib/frebby/presets/ansible.rb
@@ -5,6 +5,8 @@ require 'frebby/transform'
 
 Frebby.pluralize 'play', as: 'plays'
 Frebby.pluralize 'task', as: 'tasks'
+Frebby.pluralize 'pre_task', as: 'pre_tasks'
+Frebby.pluralize 'post_task', as: 'post_tasks'
 Frebby.pluralize 'handler', as: 'handlers'
 Frebby.pluralize 'tag', as: 'tags'
 Frebby.pluralize 'notify'
---
+++ lib/frebby/presets/terraform.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+require 'frebby/transform'
+
+Frebby.pluralize 'tag', as: 'tags'
